6a3bc034c00f35d62c11c4154101abe46fed986a
Amit Jakubowicz <amit.jakubowicz@ultimate.ai>
2021-06-13 18:17:44 +0200

    Fix build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import {getAllEventSlugs, getTheme, Event, Theme} from "../lib/datasource/datasource";
 import * as React from "react";
-import Main, {Props as MainProps} from './_main'
-import {getLandingSections} from "../lib/datasource/landing_page";
+import {getLandingSections, LandingPageSection} from "../lib/datasource/landing_page";
+import Layout from "../components/Layout";
+import LandingPage from "../components/LandingPage";
 
-export default Main
-
-
+export interface Props {
+    foo: string
+    landingSections: LandingPageSection[]
+}
+export default function Main(props: Props) {
+    return (
+        <Layout>
+            <LandingPage sections={props.landingSections} />
+        </Layout>
+    )
+}
 export async function getStaticProps(context) {
     const pages = await getLandingSections('es')
     return {
